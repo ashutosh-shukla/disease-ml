e5bff8daf3f370a49fe23f767fd76cee878b8ca7
ashutosh-shukla <121450702+ashutosh-shukla@users.noreply.github.com>
2023-11-06 18:54:29 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 
 
-diabetes_model = pickle.load(open('C:/Users/heman/OneDrive/Desktop/disease/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/heman/OneDrive/Desktop/disease/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/heman/OneDrive/Desktop/disease/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
 
 
 
